c46216d96b2e92cff34becfbde5b24d07c30e36d
eshantbist <bistishu1@gmail.com>
2020-08-30 00:22:31 +0530

    Added global bloc forms

---
+++ lib/src/screens/login_screen.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import '../blocs/bloc.dart';
+
+class LoginScreen extends StatelessWidget {
+  Widget build(context) {
+    return Container(
+      margin: EdgeInsets.all(20.0),
+      child: Column(
+        children: <Widget>[
+          emailField(),
+          passwordField(),
+          Container(
+            margin: EdgeInsets.only(top: 20.0),
+          ),
+          submitButton()
+        ],
+      ),
+    );
+  }
+}
+
+Widget emailField() {
+  return StreamBuilder(
+    stream: bloc.email,
+    builder: (context, snapshot) {
+      return TextField(
+        onChanged: bloc.changeEmail,
+        keyboardType: TextInputType.emailAddress,
+        decoration: InputDecoration(
+          hintText: 'you@example.com',
+          labelText: 'Email Address',
+          errorText: snapshot.error,
+        ),
+      );
+    },
+  );
+}
+
+Widget passwordField() {
+  return StreamBuilder(
+    stream: bloc.password,
+    builder: (context, snapshot) {
+      return TextField(
+        obscureText: true,
+        onChanged: bloc.changePassword,
+        keyboardType: TextInputType.emailAddress,
+        decoration: InputDecoration(
+          hintText: 'Password',
+          labelText: 'Password',
+          errorText: snapshot.error,
+        ),
+      );
+    },
+  );
+}
+
+Widget submitButton() {
+  return RaisedButton(
+    child: Text('Login'),
+    color: Colors.blue,
+    onPressed: () {},
+  );
+}
